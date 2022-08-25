94dffef3da610fa3e783556b8de515f1cc738233
taquangphu <102677630+taquangphu@users.noreply.github.com>
2022-08-25 14:47:01 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+﻿import requests
+from time import sleep
+
+f = open("./cron.txt", "r")
+crons = f.read().split("\n")
+
+loop = True
+phien = 1
+limit = 1
+
+while loop:
+	for cron in crons:
+		res = requests.get(cron)
+
+		print('Phiên: ' + str(phien))
+		print('+ URL: ' + cron)
+		print('+ Response: ' + res.text)
+		print('-----------------------------------')
+		phien = phien + 1
+
+	sleep(limit)
